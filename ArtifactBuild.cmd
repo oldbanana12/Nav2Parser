78175bae212cc67653e2127a9aa9e3fe1c8372ae
oldbanana12 <69821086+oldbanana12@users.noreply.github.com>
2021-01-03 16:45:10 +0000

    Fix artifact path

--- ArtifactBuild.cmd
+++ ArtifactBuild.cmd
@@ -1,7 +1,7 @@
 @echo off
 pushd "%~dp0"
 
-powershell Compress-7Zip "bin\x64\Release" -ArchiveFileName "Nav2Parser.zip" -Format Zip
+powershell Compress-7Zip "bin\x64\Release" -ArchiveFileName "Nav2ParserX64.zip" -Format Zip
 
 :exit
 popd
